4ec90c2c782acea53c79f619f5ad0ce7d31c901d
Shakthivel R <157127060+Shakthii005@users.noreply.github.com>
2024-10-12 14:02:43 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rjshkhr/cleanfolio'
+      href='#'
       className='link footer__link'
     >
       Shakthivel Rajesh.
