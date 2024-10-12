7320897502a35de213aab0c61f2f87390f0e94a3
Shakthivel R <157127060+Shakthii005@users.noreply.github.com>
2024-10-12 13:37:37 +0530

    portfolio setup

---
+++ src/portfolio.js
@@ -0,0 +1,69 @@
+const header = {
+  homepage: 'https://rjshkhr.github.io/cleanfolio',
+  title: 'Shakthivel',
+}
+
+const about = {
+  name: 'Shakthivel Rajesh',
+  role: 'Cybersecurity Specialist',
+  description: 'I am a cybersecurity specialist with a strong foundation in networking fundamentals, security information and event management (SIEM), and bash scripting. I am also skilled in effective team communication and coordination.',
+  resume: '/Resume.pdf',
+  social: {
+    linkedin: 'https://linkedin.com/in/shakthivel-rajesh',
+    github: 'https://github.com/Shakthii005',
+  },
+}
+
+const projects = [
+  {
+    name: 'Password Complexity Checker using Python',
+    description: 'A tool that evaluates password strength by identifying common patterns, checking for phone number sequences, assessing length and character variety, and providing feedback to enhance security. This project aims to promote better password practices and improve online security.',
+    stack: ['Python'],
+    sourceCode: 'https://github.com/Shakthii005/Portfolio',
+    // livePreview: 'https://github.com',
+  },
+  {
+    name: 'Dicegame using Python',
+    description: 'A simple dice game implemented in Python for 2 to 4 players. The objective is to be the first to reach a total score of 50.',
+    stack: ['Python'],
+    sourceCode: 'https://github.com/Shakthii005/Portfolio',
+    // livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  'Python',
+  'Networking Fundamentals',
+  'Security Information and Event Management (SIEM)',
+  'Bash Scripting',
+  'Effective Team Communication',
+  'Skilled Team Coordinator',
+]
+
+const education = [
+  {
+    period: '2023 - 2027',
+    institution: 'SRM UNIVERSITY, RAMAPURAM, CHENNAI',
+    degree: 'Undergraduate in Bachelor of Technology in Computer Science Engineering with Specialization in CyberSecurity',
+    gpa: '8.7 GPA',
+  },
+  {
+    period: '2021 - 2023',
+    institution: 'SBOA Matriculation and Higher Secondary School, Chennai',
+    degree: 'HSC +1, +2',
+    notable: 'SBOANZA’22 Winner (Short Film Category): Assistant Director and Distributes the winning short film Meendum Meenduma at the inter-school cultural event',
+  },
+]
+
+const certifications = [
+  'Google CyberSecurity Specialization (JUN 2024) - Coursera, Google',
+  'Networking Basics (MAR 2024) - Cisco, Netacad',
+  'Networking Device and Initial Configuration (APR 2024) - Cisco, Netacad',
+  'CS50\'s Introduction to Programming with Python (JUL 2024) - CS50, EdX',
+]
+
+const contact = {
+  email: 'svelr005@gmail.com',
+}
+
+export { header, about, projects, skills, education, certifications, contact }
